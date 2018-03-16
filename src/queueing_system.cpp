bbf146f016b88937d6b2a63248962e0d99a593fa
kargatpwnz <sibnetpwnz@yandex.ru>
2018-03-16 16:26:25 +0700

    Added queueing system

--- src/queueing_system.cpp
+++ src/queueing_system.cpp
@@ -2,4 +2,107 @@
 // Created by na1l on 04.03.18.
 //
 
+#include <iostream>
 #include "queueing_system.h"
+
+QueueingSystem::QueueingSystem() {
+  FillClients();
+  FillEvents();
+  event_start_ = event_list_;
+}
+
+void QueueingSystem::FillClients() {
+  clients_[0].id = 0;
+  clients_[0].time_in = 2.2;
+  clients_[0].time_out = 2.7;
+
+  clients_[1].id = 1;
+  clients_[1].time_in = 1.4;
+  clients_[1].time_out = 0.7;
+
+  clients_[2].id = 2;
+  clients_[2].time_in = 1.6;
+  clients_[2].time_out = 1.2;
+}
+
+void QueueingSystem::FillEvents() {
+  for (auto client : clients_) {
+    struct event event;
+
+    event.client_id = client.id;
+    event.time = client.time_in;
+    event.type = enum_Event::EVENT_ENQUEUE;
+
+    if (event_list_.empty()) {
+      event_list_.insert(event);
+    } else {
+      event.time = client.time_in + event_list_.rbegin()->time;
+      event_list_.insert(event);
+    }
+  }
+}
+
+void QueueingSystem::PrintEventList() {
+  std::cout << "ID\tTIME\tTYPE" << std::endl;
+  for (auto _event : event_list_) {
+    std::cout << _event.client_id << "\t"
+              << _event.time << "\t\t"
+              << _event.type << std::endl;
+
+  }
+}
+
+void QueueingSystem::Start() {
+  int running = clients_[0].id;
+
+  for (auto event : event_start_) {
+    struct event e;
+    int id = event.client_id;
+
+    if (running != id && event.type == enum_Event::EVENT_ENQUEUE) {
+      queue_.push(id);
+      continue;
+    }
+
+    if (event.client_id == running &&
+        event.type == enum_Event::EVENT_DEQUEUE) {
+
+      if (!queue_.empty()) {
+        running = queue_.front();
+        queue_.pop();
+      }
+
+      if (event.type == enum_Event::EVENT_DEQUEUE &&
+          event.client_id == event_start_.rbegin()->client_id) {
+
+        e.time = event.time + clients_[running].time_out;
+        e.client_id = running;
+        e.type = enum_Event::EVENT_DEQUEUE;
+
+        event_start_.insert(e);
+        break;
+      }
+      e.time = event.time + clients_[running].time_out;
+      e.client_id = running;
+      e.type = enum_Event::EVENT_DEQUEUE;
+
+      event_start_.insert(e);
+      continue;
+    }
+
+    e.client_id = id;
+    e.time = event.time + clients_[id].time_out;
+    e.type = enum_Event::EVENT_DEQUEUE;
+    event_start_.insert(e);
+
+  }
+}
+void QueueingSystem::PrintStartList() {
+  std::cout << "ID\tTIME\tTYPE" << std::endl;
+  for (auto _event : event_start_) {
+    std::cout << _event.client_id << "\t"
+              << _event.time << "\t\t"
+              << _event.type << std::endl;
+
+  }
+}
--- src/queueing_system.h
+++ src/queueing_system.h
@@ -5,7 +5,62 @@
 #ifndef MODELING_QUEUEING_SYSTEM_H
 #define MODELING_QUEUEING_SYSTEM_H
 
+#include <set>
+#include <queue>
+
+enum Time {
+  TIME_CONST,
+  TIME_EQUAL,
+  TIME_EXPONENT
+};
+
+enum enum_Event {
+  EVENT_ENQUEUE,
+  EVENT_DEQUEUE
+};
+
+struct client {
+  double time_in;
+  double time_out;
+  double queueing_time;
+
+  int id;
+};
+
+struct event {
+  double time;
+  int client_id;
+
+  int type;
+
+  friend bool operator<(const event &e1, const event &e2) {
+    return e1.time < e2.time;
+  }
+
+  friend bool operator==(const event &e1, const event &e2) {
+    return e1.time == e2.time &&
+        e1.client_id == e2.client_id &&
+        e1.type == e2.type;
+  }
+
+};
+
 class QueueingSystem {
+ public:
+  QueueingSystem();
+  void PrintEventList();
+  void Start();
+  void PrintStartList();
+
+ private:
+  void FillClients();
+  void FillEvents();
+
+  struct client clients_[3];
+
+  std::set<struct event> event_list_;
+  std::set<struct event> event_start_;
+  std::queue<int> queue_;
 
 };
 
